425cc03f512e00b9e0e9ef7cc80724ed48e40c18
18560022393 <jac0625@163.com>
2018-04-11 19:21:31 +0800

    4.11

--- src/main/java/com/lechenggu/bkeasygo/dao/IUserDAO.java
+++ src/main/java/com/lechenggu/bkeasygo/dao/IUserDAO.java
@@ -42,7 +42,7 @@ public interface IUserDAO {
      *
      * @return 用户对象
      */
-    Manager findById();
+    Manager findById(int id);
 
 
 }
---
+++ src/test/com/lechenggu/bkeasygo/dao/IUserDAOTest.java
@@ -0,0 +1,28 @@
+package com.lechenggu.bkeasygo.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IUserDAOTest {
+
+    @Test
+    public void doCreate() {
+    }
+
+    @Test
+    public void doRemove() {
+    }
+
+    @Test
+    public void doUpdate() {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findById() {
+    }
+}
